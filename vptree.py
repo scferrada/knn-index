5e27c7595986470cec9f4270bf5bee04633eb973
Sebastián Ferrada <scferradaa@gmail.com>
2018-12-11 15:45:52 -0300

    search method

--- vptree.py
+++ vptree.py
@@ -1,4 +1,5 @@
 import numpy as np
+from heapq import heappush, heappushpop
 
 '''
 This Class holds the nodes of a vantage-point tree.
@@ -16,13 +17,38 @@ class Node:
 		self.mu = mu
 		self.minp = minp
 		self.maxp = maxp
+	
+	def is_leaf(self):
+		return False
 		
 class Leaf:
-	def __init__(self, points)
+	def __init__(self, points, minp, maxp)
 		self.points = points
+		self.minp = minp
+		self.maxp = maxp
+	
+	def is_leaf(self):
+		return True
 
 def manhattan(D, x):
 	return np.sum(np.abs(D-x), axis=1)
+	
+def manhattan2(x, y):
+	return np.sum(np.abs(y[1:]-x[1:]))
+	
+class Res:
+    def __init__(self, obj, dist):
+        self.dist = -dist
+        self.obj = obj
+
+    def __lt__(self, other):
+        return self.dist > other.dist
+
+    def __eq__(self, other):
+        return self.dist == other.dist and self.obj == other.obj
+
+    def __str__(self):
+        return str(self.obj) + "; " + str(self.dist)
 		
 '''
 chooses a better-than-random vantage-point,
@@ -40,7 +66,7 @@ def select_vp(data, distance):
 		if spread > best_spread:
 			best_spread = spread
 			vp = pi
-	return vp, np.vstack((data[:vp[0]], data[vp[0]+1:]))
+	return vp, np.vstack((data[:vp[0]], data[vp[0]+1:])) #no funciona
 		
 '''
 This method buids a vantage-point tree, given a numpy matrix.
@@ -51,9 +77,9 @@ def build_tree(data, c=1, distance=manhattan):
     matrix = np.hstack((idx, data))
 	return build_tree_internal(matrix, c, distance)
 
-def build_tree_internal(data, c= 1, distance=manhattan, minP=0, maxP=0):
+def build_tree_internal(data, c=1, distance=manhattan, minP=0, maxP=0):
 	if len(data) <= c:
-		return Leaf(data)
+		return Leaf(data, minP, maxP)
 	vp, data = select_vp(data, distance)
 	distances = distance(data[:,1:], vp[1:])
 	mu = np.median(distances)
@@ -65,3 +91,36 @@ def build_tree_internal(data, c= 1, distance=manhattan, minP=0, maxP=0):
 	R = build_tree_internal(data[distances >= mu], c, distance, _minR, _maxR)
 	return Node(L, R, vp, mu, minP, maxP)
 	
+'''
+This method searches for the object k nearest neighbours of 
+x on a vp-tree starting at node n
+'''
+def search(n, x, k, distance=manhattan2):
+	return search_internal(n, x, k, distance, [])
+	
+def search_internal(n, x, k, distance, results):
+	if n.is_leaf():
+		for p in n.points:
+			d = distance(x, p)
+			if len(results) < k:
+				heappush(results, Res(p[0], d))
+			elif d < results[0].dist:
+				heappushpop(results, Res(p[0], d)
+		#return results
+	d = distance(n.vp, x)
+	if len(results) < k:
+		heappush(results, Res(p[0], d))
+	elif d < results[0]:
+		heappushpop(results, Res(p[0], d)		
+	middle = (n.L.maxp + n.R.minp)/2
+	if d < middle:
+		if n.L.minp - results[0].dist < d and d < n.L.maxp + results[0].dist:
+			search_internal(n.L, x, k, distance, results)
+		if n.R.minp - results[0].dist < d and d < n.R.maxp + results[0].dist:
+			search_internal(n.R, x, k, distance, results)
+	else:
+		if n.R.minp - results[0].dist < d and d < n.R.maxp + results[0].dist:
+			search_internal(n.R, x, k, distance, results)
+		if n.L.minp - results[0].dist < d and d < n.L.maxp + results[0].dist:
+			search_internal(n.L, x, k, distance, results)
+	#return results
